fd7906d3690225b220e63287d73879ee3ff5292e
github_UlisesVil <ulisesvil5@hotmail.com>
2021-09-24 21:19:32 -0500

    Templates with style

--- templates/angularProjectsMainPage.js
+++ templates/angularProjectsMainPage.js
@@ -7,14 +7,17 @@ const angularProjectsAppMainPage=
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Backend Angular-Apps</title>
-        <link rel="icon" type="image/x-icon" href="">
+        <link rel="icon" type="image/x-icon" href="https://www.crossroadsfund.org/sites/default/files/document%20icon.png">
         <style>
             body{
-                background:#181818;
-                color: white;
                 display:flex;
                 justify-content:center;
                 align-items:center;
+                background: url(https://get.wallhere.com/photo/lights-city-street-cityscape-night-cyberpunk-skyscraper-evening-bar-metropolis-light-color-downtown-screenshot-urban-area-16259.jpg);
+                background-attachment: fixed;
+                background-position: center;
+                background-size: cover;
+                color: white;
             }
             .content{
                 display:flex;
@@ -22,11 +25,16 @@ const angularProjectsAppMainPage=
                 align-items:center;
                 justify-content:center;
                 width:90%;
+                border: 3px solid purple;
+                border-radius: 10px;
+                padding-bottom: 20px;
                 margin: auto;
                 text-align: center;
+                text-shadow: 3px 3px 5px black;
+                background: rgba(0,0,0,.8);
             }
             img{
-                width:40%;
+                width:20%;
                 min-width:200px;
                 margin-top:30px;
             }
@@ -34,12 +42,15 @@ const angularProjectsAppMainPage=
                 text-decoration:none;
                 color: yellowgreen;
             }
+            a:hover{
+                color: purple;
+            }
         </style>
     </head>
     <body>
         <div class="content">
             <h1 style='color: white;'>Welcome!! This is the API Rest from my Angular Projects</h1>
-            <h2>You can visit my Git repositories here: <a href="https://ulisesvil.github.io" target="_blank">Ulises Villa - Angular Projects</a></h2>
+            <h2>You can visit my Git repositories here: <a href="https://github.com/UlisesVil" target="_blank">Ulises Villa - GitHub</a></h2>
             <img src="https://cdnb.artstation.com/p/assets/images/images/021/188/673/original/pixel-jeff-meow-s-sushi-600.gif?1570717603">
         </div>
     </body>
--- templates/chartsMainPage-template.js
+++ templates/chartsMainPage-template.js
@@ -2,45 +2,57 @@
 
 const chartAppMainPage=
 `<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Charts-App</title>
-        <link rel="icon" type="image/x-icon" href="">
-        <style>
-            body{
-                background:#181818;
-                color: white;
-                display:flex;
-                justify-content:center;
-                align-items:center;
-            }
-            .content{
-                display:flex;
-                flex-direction:column;
-                align-items:center;
-                justify-content:center;
-                width:90%;
-                margin: auto;
-                text-align: center;
-            }
-            img{
-                width:40%;
-                min-width:200px;
-                margin-top:30px;
-            }
-            a{
-                text-decoration:none;
-                color: yellowgreen;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="content">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend HighCharts Angular App</title>
+    <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/UlisesVil/HighChartsAngularApp/main/src/assets/icons/graphIcon01.jpg">
+    <style>
+        body{
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            background: url(https://blogs.imf.org/wp-content/uploads/2017/08/BLOG-1024x600-image-of-charts-TOP5Charts-iStock-615507200.jpg);
+            background-attachment: fixed;
+            background-position: center;
+            background-size: cover;
+            color: white;
+        }
+        .content{
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            justify-content:center;
+            width:90%;
+            border: 3px solid purple;
+            border-radius: 10px;
+            padding-bottom: 20px;
+            margin: auto;
+            text-align: center;
+            text-shadow: 3px 3px 5px black;
+            background: rgba(0,0,0,.8);
+        }
+        img{
+            width:20%;
+            min-width:200px;
+            margin-top:30px;
+        }
+        a{
+            text-decoration:none;
+            color: yellowgreen;
+        }
+        a:hover{
+            color: purple;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
             <h1 style='color: white;'>Welcome!! This is the API Rest from The Charts Angular APP</h1>
-            <h2>You can visit the site here: <a href="https://ulisesvil.github.io" target="_blank">Charts Angular APP</a></h2>
-            <img src="https://images-cdn.newscred.com/Zz04NjA3ZjljMjQ0ODkxMWViOWRjYzU1OGJkNjI1ZjVkZA==">
+            <h2>You can visit the repo here: <a href="https://github.com/UlisesVil/HighCartsNodeJSBackend" target="_blank">GitHub Repo</a></h2>
+            <h2>And the App here: <a href="https://ulisesvil.github.io/HighChartsAngularApp" target="_blank">Charts Angular APP</a></h2>
+            <img src="https://raw.githubusercontent.com/UlisesVil/HighChartsAngularApp/main/src/assets/icons/piesample.jpg">
         </div>
     </body>
 </html>`;
